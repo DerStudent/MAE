39d218543f176063fc66a758523a877e5c3efe14
unknown <dustin.pawlowicz001@stud.fh-dortmund.de>
2018-05-28 21:11:34 +0200

    add transaction activity design changes. Dustin

--- app/src/main/java/de/fh/mae/md2/app/AddTransaction.java
+++ app/src/main/java/de/fh/mae/md2/app/AddTransaction.java
@@ -3,26 +3,49 @@ package de.fh.mae.md2.app;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Paint;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class AddTransaction extends AppCompatActivity {
+public class AddTransaction extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_transaction);
 
+        View current = getCurrentFocus();
+        if (current != null) {
+            current.clearFocus();
+        }
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setOnClickListeners();
 
-        Button amountButton = (Button) findViewById(R.id.transaction_amount);
+        TextView amountText = (TextView) findViewById(R.id.transaction_amount_text);
         String currency = String.format(getResources().getString(R.string.add_transaction_currency));
-        amountButton.setText("0,00 " + currency);
+        amountText.setText("0,00 " + currency);
 
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        TextView categoryText = (TextView) findViewById(R.id.transaction_category_text);
+        categoryText.setText("Supermarkt");
+
+        TextView calendarText = (TextView) findViewById(R.id.transaction_calendar_text);
+        calendarText.setText("Heute");
+    }
+
+    private void setOnClickListeners() {
+        RelativeLayout addTransactionAmount = (RelativeLayout) findViewById(R.id.layout_add_transaction_amount);
+        addTransactionAmount.setOnClickListener(this);
+        RelativeLayout addTransactionCategory = (RelativeLayout) findViewById(R.id.layout_add_transaction_category);
+        addTransactionCategory.setOnClickListener(this);
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.action_floatingActionButton);
     }
 
     @Override
@@ -37,6 +60,17 @@ public class AddTransaction extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.add_transaction, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int i = view.getId();
+
+        if (i == R.id.layout_add_transaction_amount) {
+
+        } else if (i == R.id.layout_add_transaction_category) {
+
+        }
     }
 
 }
