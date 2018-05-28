230ce39f17cb004bac76e75e3b42e15b96b67f6b
DerStudent <strolsu@googlemail.com>
2018-05-28 16:46:26 +0100

    Activity für Kalender und PieCHart

---
+++ app/src/main/java/de/fh/mae/md2/app/PieChartActivity.java
@@ -0,0 +1,38 @@
+package de.fh.mae.md2.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PiChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        PieChart chart = (PieChart) findViewById(R.id.chart1);
+
+        float[] werte = new float[5];
+        werte[0] = 20.0f;
+        werte[1] = 45.0f;
+        werte[2] = 2.0f;
+        werte[3] = 15.0f;
+        werte[4] = 30.0f;
+
+        List<PieEntry> list = new ArrayList<>();
+
+        for (float wert : werte) {
+            list.add(new PieEntry(wert));
+        }
+
+        PieDataSet dataset = new PieDataSet(list, "PiChart");
+    }
+}
